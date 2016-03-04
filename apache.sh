56d4a4e3eb5f011ac4c8b1522be837e8d4b51428
peterp4n <peterp4n@gmail.com>
2016-03-04 16:33:18 +0900

    Create apache.sh

---
+++ apache.sh
@@ -0,0 +1,27 @@
+sed -i "s/#define DEFAULT_SERVER_LIMIT 256/#define DEFAULT_SERVER_LIMIT 1024/g" server/mpm/prefork/prefork.c
+sed -i "s/#define DEFAULT_SERVER_LIMIT 16/#define DEFAULT_SERVER_LIMIT 64/g" server/mpm/worker/worker.c
+sed -i "s/User daemon/User apache/g" docs/conf/httpd.conf.in
+sed -i "s/Group daemon/Group apache/g" docs/conf/httpd.conf.in
+sed -i "s/#ServerName www.example.com:@@Port@@/ServerName 218.146.255.209/g" docs/conf/httpd.conf.in
+./configure \
+--prefix=/web/apache \
+--with-layout=Apache \
+--with-included-apr \
+--with-pcre \
+--enable-modules=all \
+--enable-mods-shared=all \
+--enable-vhost-alias \
+--enable-mime-magic \
+--enable-usertrack \
+--enable-speling \
+--enable-deflate \
+--enable-rewrite \
+--enable-info \
+--enable-ssl \
+--enable-so
+make -j `grep -c ^processor /proc/cpuinfo`
+make install
+
+#--enable-proxy \
+#--enable-proxy-http \
+#--with-mpm=worker \
