1e370a2333687fefdd0dcc3142078dd6af2c1a10
peterp4n <peterp4n@gmail.com>
2016-03-19 17:54:06 +0900

    Create nginx.sh

---
+++ nginx.sh
@@ -0,0 +1,15 @@
+./configure \
+--with-http_stub_status_module \
+--without-http_upstream_ip_hash_module \
+--without-http_gzip_module \
+--without-http_autoindex_module \
+--without-http_geo_module \
+--without-http_map_module \
+--without-http_split_clients_module \
+--without-http_referer_module \
+--without-http_proxy_module \
+--without-http_uwsgi_module \
+--without-http_limit_zone_module \
+--without-http_limit_req_module \
+--without-http_browser_module \
+
