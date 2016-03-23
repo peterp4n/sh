cabb6d615c0ab21cdef9b68bef3985a2e30a81c8
peterp4n <peterp4n@gmail.com>
2016-03-23 10:24:52 +0900

    Update phpredis7.sh

--- phpredis7.sh
+++ phpredis7.sh
@@ -1,7 +1,7 @@
 wget https://github.com/phpredis/phpredis/tree/php7 -O phpredis7.zip
 
 /configure \
---with-php-config=/web/php5/bin/php-config \
+--with-php-config=/web/php7/bin/php-config \
 --enable-redis \
 --enable-redis-igbinary \
 --enable-redis-session \
