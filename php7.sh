1f33abd4faafd4ad55b1e3477a0d00a8ded86f16
peterp4n <peterp4n@gmail.com>
2017-02-23 11:40:37 +0900

    Update php7.sh

--- php7.sh
+++ php7.sh
@@ -75,3 +75,23 @@ rm -fr configure
 #--enable-xdebug \
 # ext/redis 추가  
 # yum -y install openssh* libssh* openssl* net-snmp* libxslt* re2c*
+
+timezone = Asia/Seoul
+include_path = ".:/web/php7/lib/medoo:/web/php7/lib/predis:/web/php7/lib/phpMailer:/web/php7/lib/mobile:/web/php7/lib/Snoopy"
+
+http://browscap.org/stream?q=PHP_BrowsCapINI
+https://github.com/PHPMailer/PHPMailer/
+http://mobiledetect.net
+http://medoo.in
+https://sourceforge.net/projects/snoopy/
+https://github.com/nrk/predis
+
+[opcache]
+zend_extension=/web/php5/lib/php/extensions/no-debug-non-zts-20131226/opcache.so
+
+[igbinary]
+extension=/web/php5/lib/php/extensions/no-debug-non-zts-20131226/igbinary.so
+
+[redis]
+extension=/web/php5/lib/php/extensions/no-debug-non-zts-20131226/redis.so
+                                                                               
