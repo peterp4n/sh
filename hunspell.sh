31935070d5d83acb8e1c3b6fbcb8957cbe20a4b1
peterp4n <peterp4n@gmail.com>
2017-05-02 10:34:57 +0900

    Create hunspell.sh

---
+++ hunspell.sh
@@ -0,0 +1,40 @@
+
+#### https://taegon.kim/archives/2551
+
+
+###. hunspell 
+1. down 
+https://github.com/hunspell/hunspell
+2. install
+autoreconf -vfi
+./configure --prefix=/home/server/hunspell
+make
+mkae install
+3. test
+
+###. 한국어사전
+1. down 
+https://github.com/changwoo/hunspell-dict-ko/
+2. python3 install 
+# wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tgz
+# ./configure --prefix=/home/server/python3 
+# ./configure --enable-optimizations
+# make
+# make altinstall
+# strip /home/server/python3/lib/libpython3.6m.so.1.0
+3. pip설치  
+# wget https://bootstrap.pypa.io/get-pip.py 
+python3.6 get-pip.py
+# /home/server/python3/bin/python --version
+
+
+
+###. php_hunspell
+1. down
+https://github.com/taggon/php_hunspell
+2. install
+# /home/server/php7/bin/phpize
+# ./configure --with-php-config=/home/server/php7/bin/php-config
+# make
+# make install
+
