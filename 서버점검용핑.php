5f452e57228c0e65b011a11574b994a107d86b38
peterp4n <peterp4n@gmail.com>
2017-11-07 11:42:13 +0900

    Create 서버점검용핑.php

---
+++ 서버점검용핑.php
@@ -0,0 +1,13 @@
+function ping($host,$port=80,$timeout=6)
+{
+        $fsock = fsockopen($host, $port, $errno, $errstr, $timeout);
+        if ( ! $fsock )
+        {
+                return FALSE;
+        }
+        else
+        {
+        fclose($fsock);
+                return TRUE;
+        }
+}
