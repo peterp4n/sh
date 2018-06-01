86f5f59b18e39ad33badb0e9041f6d1939f4378c
peterp4n <peterp4n@gmail.com>
2018-06-01 18:00:21 +0900

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,1 +1,11 @@
-working
+1. download
+   https://nodejs.org/en/download/ 
+   
+2. install
+  tar zxvf node-v*.tar.gz
+  cd node-v*
+  ./configure --prefix=/web/nodejs
+  make 
+  make install
+  
+3.   
