7d28ddff1cb63a52d2ca20fe5cc2c778baf13cb0
peterp4n <peterp4n@gmail.com>
2018-06-08 13:03:37 +0900

    Update composer.sh

--- composer.sh
+++ composer.sh
@@ -1,1 +1,1 @@
- curl -sS https://getcomposer.org/installer | /web/php7bin/php && mv composer.phar /usr/bin/composer
+ curl -sS https://getcomposer.org/installer | /web/php7/bin/php && mv composer.phar /usr/bin/composer
