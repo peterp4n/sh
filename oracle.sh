a39abc53a071766755ddcec1af7a6354e4b63a99
peterp4n <peterp4n@gmail.com>
2018-09-28 18:50:15 +0900

    Create oracle.sh

---
+++ oracle.sh
@@ -0,0 +1,13 @@
+0. oracle download
+   http://www.oracle.com
+   
+1. oracle install
+   http://raccoonjy.tistory.com/19?category=748711
+   
+2. database create
+
+   Database Configuration Assistant
+   http://raccoonjy.tistory.com/20
+
+3. listener assitant
+   http://raccoonjy.tistory.com/21?category=748711
