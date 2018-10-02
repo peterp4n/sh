c51b6b40e305aca6982017c3d6a5e96a076c90b2
peterp4n <peterp4n@gmail.com>
2018-10-02 16:34:36 +0900

    Create mailgun_use.sh

---
+++ mailgun_use.sh
@@ -0,0 +1,10 @@
+
+https://www.mailgun.com/
+
+http://recordingbetter.com/aws/2017/09/22/custom-domain-email-gmail-mailgun
+
+https://hinco114.github.io/2017/07/05/mailgun/
+
+https://starrysky.co.kr/2017/11/%EA%B0%80%EB%B3%8D%EA%B2%8C-%EC%93%B0%EB%8A%94-%EC%82%BD%EC%A7%88%EA%B8%B0-mailgunmanaged-email-service/
+
+http://wequlo.blogspot.com/2017/04/mail-gun.html
