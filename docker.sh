8f53208a4ea097720948b9f3948ff0ba6e300757
peterp4n <peterp4n@gmail.com>
2018-11-12 10:53:07 +0900

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,9 @@
+
+
+### docker-compose
+1. install
+    참조 : https://docs.docker.com/compose/install/#install-compose
+    # sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o //bin/docker-compose
+    # chmod +x /bin/docker-compose
+    
+    
