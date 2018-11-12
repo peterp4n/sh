d5a909aafc95ab343c3ac840def712b8e9a1daa3
peterp4n <peterp4n@gmail.com>
2018-11-12 13:06:02 +0900

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -58,5 +58,52 @@
 	docker tag 내컴퓨터에있는이미지명 계정명/생성할이미지명:생성할태그명
 
   
+	#도커hub에 이미지 업로드
+	docker push 계정명/이미지명:태그명
+
+	#도커서비스 목록
+	docker service ls
+	docker service ps 서비스명
+
+	#도커 swam시작하기(라운드로빈가능)
+	docker swarm init
+
+	#docker swam join문을 실행하여 다른 컴퓨터와 연결할 수 있다.
+	docker swarm join --token 토큰키 아이피:포트
+
+	#docker yml설정 파일 기반으로 swam실행하고 이름 지정하기
+	docker stack deploy -c docker-compose.yml swam생성이름
+	#docker-compose.yml 예제
+		version: "3"
+		services:
+			web:
+				# replace username/repo:tag with your name and image details
+				image: username/repo:tag
+				deploy:
+					replicas: 5
+					resources:
+						limits:
+							cpus: "0.1"
+							memory: 50M
+					restart_policy:
+						condition: on-failure
+				ports:
+					- "80:80"
+				networks:
+					- webnet
+		networks:
+			webnet:
+
+	#docker swam 서비스중인 목록
+	docker service ls
+
+	#docker swam 이름으로 실행중인 서비스 목록 보기
+	docker service ps swam생성이름
+
+	#docker swam 종료
+	docker swarm leave --force
+
+	#docker swam 제거
+	docker stack rm swam생성이름
 
  
