57c0d17c62a20461ac6b32cca35ba8e489d5fefd
peterp4n <peterp4n@gmail.com>
2018-11-13 13:18:44 +0900

    Update node.sh

--- node.sh
+++ node.sh
@@ -17,3 +17,57 @@
   cd project
   vi package.json
   project working
+
+## process 관리 PM2
+1. 설치 
+$ npm install pm2 -g
+
+2. 실행 -(fork / cluster)
+$ pm2 start server.js --name "서버이름"   -- fork(일반적인 방식) 
+$ pm2 start app.js -i 0 --name "서버이름" -- cluster
+
+3. 목록
+$ pm2 list
+
+4. 상세
+$ pm2 show 서버이름
+
+5. 정지
+$ pm2 stop 서버이름
+
+6. 재시작
+$ pm2 restart 서버이름
+
+7. 로그파일 확인
+$ pm2 logs 서버이름
+
+8. 모니터링
+$ pm2 monit
+
+9. json 실행
+$ pm2 start 서버이름.json
+
+@@ 서버이름.json
+{
+  "apps": [{
+    "name": "example",
+    "script": "서버이름.js",
+    "watch": false,
+    "env": {
+      "NODE_ENV": "production",
+      "API_PORT": 4000
+    },
+    "exec_mode": "cluster",
+    "instances": 0
+  }]
+}
+
+
+
+
+
+
+
+
+
+
