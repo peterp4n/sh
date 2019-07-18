5fc8b3a85bc0f66a4b94e8db4dc4c64138247c70
peterp4n <peterp4n@gmail.com>
2019-07-18 13:36:13 +0900

    Create ffmpeg.sh

---
+++ ffmpeg.sh
@@ -0,0 +1,6 @@
+## ffmpeg package install with yum on centos7
+1. yum intall -y re2c
+2. yum -y install epel-release 
+3. rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
+3. yum -y install x264 x264-libs x264-devel
+4. yum -y install ffmpeg ffmpeg-devel
