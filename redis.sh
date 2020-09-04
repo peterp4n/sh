f3d882b43665593950b0b796aa48df2c22ba830b
peterp4n <peterp4n@gmail.com>
2020-09-04 11:43:09 +0900

    Update redis.sh

--- redis.sh
+++ redis.sh
@@ -1,3 +1,4 @@
+## redis server
 1. download
 wget http://download.redis.io/releases/redis-3.2.8.tar.gz
 
@@ -15,3 +16,14 @@ vi /etc/init.d/redis_6379 -> 설치경로설정
 
 5. 실행
 /etc/init.d start
+
+
+## redis php client
+
+wget https://github.com/phpredis/phpredis/tree/php7 -O phpredis7.zip
+
+/configure \
+--with-php-config=/web/php7/bin/php-config \
+--enable-redis \
+--enable-redis-igbinary \
+--enable-redis-session \
