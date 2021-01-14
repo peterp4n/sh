58c9c29f7affc5195f5be7362d7b06162225273a
peterp4n <peterp4n@gmail.com>
2021-01-14 11:41:23 +0900

    Update 2021_riro.sh

--- 2021_riro.sh
+++ 2021_riro.sh
@@ -1,51 +1,81 @@
+
+######################################
+#
+# dev-db
+# redis install
+# mariadb install
+#
+######################################
 yum -y update
+## timezone set
 echo "UTC=true" >> /etc/sysconfig/clock
 echo "ZONE=\"Asia/Seoul\"" > /etc/sysconfig/clock
 ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
+## amazon-linux-extras install
+yum install -y amazon-linux-extras
+amazon-linux-extras disable docker
+amazon-linux-extras enable lamp-mariadb10.2-php7.2 redis4.0
+yum install -y mariadb-server
+systemctl start mariadb
+mysql_secure_installation
+systemctl enable mariadb
+yum install -y redis redis-devel
+systemctl enable redis
+systemctl start redis
+
+
+######################################
+#
+# httpd install
+# php install
+#
+######################################
+yum -y update
+## timezone set
+echo "UTC=true" >> /etc/sysconfig/clock
+echo "ZONE=\"Asia/Seoul\"" > /etc/sysconfig/clock
+ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
+## java 1.8 install
+yum -y install java-1.8.0
+alternatives --config java
 yum -y groupinstall 'Development Tools'
-yum -y install openssl openssl-devel mod24_ssl httpd24 php72 php72-common php72-devel mysql57-server php72-pecl-scrypt 
-yum -y install php72-bcmath php72-cli php72-dba php72-dbg php72-enchant php72-fpm php72-gd php72-gmp php72-intl php72-json php72-mbstring php72-mysqlnd php72-opcache php72-pdo php72-pecl-apcu php72-pecl-apcu-devel php72-pecl-igbinary php72-pecl-igbinary-devel php72-pecl-imagick php72-pecl-imagick-devel php72-pecl-mcrypt php72-pecl-oauth php72-pecl-redis php72-pecl-ssh2 php72-pecl-uuid php72-pecl-xdebug php72-pgsql php72-process php72-pspell php72-recode php72-snmp php72-soap php72-tidy php72-xml php72-xmlrpc 
+## amazon-linux-extras install
+#yum install -y amazon-linux-extras
+amazon-linux-extras disable docker
+amazon-linux-extras enable epel httpd_modules php7.4
+yum -y install epel-release
+yum -y install s3fs-fuse
+## aws-cli
+yum -y install aws-cli
+echo "access key:secret key" > /etc/password-s3fs
+vi /etc/fuse.conf
+aws configure
+s3fs -o allow_other -o umask=0002 rirodata /web/rirodata
+git config --global credential.helper '!aws codecommit credential-helper $@'
+git config --global credential.UseHttpPath true
+# web server install
+yum install -y httpd httpd-tools mod_ssl ImageMagick ImageMagick-devel
 sed -i "s/#ServerName www.example.com:80/ServerName localhost:80/g" /etc/httpd/conf/httpd.conf
-sed -i "s/LoadModule http2_module modules\/mod_http2.so/#LoadModule http2_module modules\/mod_http2.so/g" /etc/httpd/conf.modules.d/00-base.conf
+sed -i "s/LoadModule http2_module modules\/mod_http2.so/#LoadModule http2_module modules\/mod_http2.so/g" /etc/httpd/conf.modules.d/10-h2.conf
 sed -i "s/LoadModule lbmethod_heartbeat_module modules\/mod_lbmethod_heartbeat.so/#LoadModule lbmethod_heartbeat_module modules\/mod_lbmethod_heartbeat.so/g" /etc/httpd/conf.modules.d/00-proxy.conf
-
-### scrypt install 
-yum -y install php7-pear
-pecl7 install scrypt
-echo '' >> /etc/php.ini
-echo ';scrypt' >> /etc/php.ini
-echo 'extension=scrypt.so' >> /etc/php.ini
-
-
+sudo systemctl enable httpd 
+sudo systemctl start httpd 
+yum -y install php php-devel php-common php-cli php-mysqlnd php-pdo php-redis php-gd php-opcache php-pear php-mbstring
+pecl install xdebug
+pecl install imagick
+pecl install scrypt
 ## php.ini edit
-display_errors = Off/display_errors = On
-;date.timezone =/date.timezone = Asia/Seoul
-short_open_tag = Off/short_open_tag = On
+echo "zend_extension=/usr/lib64/php/modules/xdebug.so" >> /etc/php.ini
+echo "extension=scrypt.so" >> /etc/php.ini
+echo "extension=imagick.so" >> /etc/php.ini
+sed -i "s/display_errors = Off/display_errors = On/g" /etc/php.ini
+sed -i "s/;date.timezone =/date.timezone = Asia\/Seoul/g" /etc/php.ini
+sed -i "s/short_open_tag = Off/short_open_tag = On/g" /etc/php.ini
 
 # composer install
 curl -s https://getcomposer.org/installer | php
 mv composer.phar /usr/bin/composer
-
-service httpd start
-chkconfig httpd on
-chkconfig --list httpd
-
-
-## aws-cli
-yum -y install aws-cli
-
-
-## java 1.8 install
-yum -y install java-1.8.0
-alternatives --config java
-
-
-### mysql install
-yum -y install mysql-server
-service mysqld start
-chkconfig mysqld on
-
-
+     
 ### redis install
 yum -y install jemalloc jemalloc-devl lua-devel
 wget http://download.redis.io/redis-stable.tar.gz
@@ -62,9 +92,7 @@ Executable     : /usr/local/redis/bin/redis-server
 cd /usr/local/redis/bin/
 vi redis.status
 ./redis.status start
-
-
-
+ 
 ## codecommit 자격증명충돌로 403 오류 발생시
 https://docs.aws.amazon.com/codecommit/latest/userguide/setting-up-https-unixes.html#setting-up-https-unixes-credential-helper
 $ git config --global credential.helper '!aws codecommit credential-helper $@'
