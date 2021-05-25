9cfadefd2e5b19975ab7d3debcdcb9aa6adda113
peterp4n <peterp4n@gmail.com>
2021-05-25 14:17:52 +0900

    Update 아마존 용량증가.sh

--- 아마존 용량증가.sh
+++ 아마존 용량증가.sh
@@ -3,8 +3,14 @@
 2. Linux 파일 시스템 확장 
 $ lsblk
 $ lsblk -f
-$ sudo growpart /dev/xvdf 1
+$ sudo growpart /dev/xvda 1
 $ lsblk
-$ resize2fs /dev/nvme0n1p1
+
+## 파일시스템  ext2, ext3, ext4 일때 사용
+$ resize2fs /dev/xvda1 
+
+## 파일시스템 xfs 일때 사용
+$ xfs_growfs /dev/xvda1 
+
 $ df -h
 $ fdisk -l
