a40aa8f2547c5668e01b95f90a3164af4ff603a7
peterp4n <peterp4n@gmail.com>
2022-02-18 11:30:31 +0900

    Update 아마존 용량증가.sh

--- 아마존 용량증가.sh
+++ 아마존 용량증가.sh
@@ -1,10 +1,16 @@
+
+0.  참고 
+https://docs.aws.amazon.com/ko_kr/AWSEC2/latest/UserGuide/recognize-expanded-volume-linux.html
 1. aws.amazon.com 에서 볼륨 용량 확장
+   ec2 대시보드 > EBS > 볼륨 > ebs 선택 > 볼륨 수정 > 크기 수정
 
 2. Linux 파일 시스템 확장 
+$ df -hT
 $ lsblk
 $ lsblk -f
 $ sudo growpart /dev/xvda 1
 $ lsblk
+$ xfs_growfs -d /
 
 ## 파일시스템  ext2, ext3, ext4 일때 사용
 $ resize2fs /dev/xvda1 
@@ -14,3 +20,4 @@ $ xfs_growfs /dev/xvda1
 
 $ df -h
 $ fdisk -l
+
